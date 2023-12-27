1b65bffec139e98ac16f0d0c5e25cc3ed233b425
unknown <sakarim9124@gmail.com>
2023-12-27 11:57:33 +0530

    updated

--- client/src/screens/auth/AddRooms.js
+++ client/src/screens/auth/AddRooms.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, InputNumber, Button } from "antd";
+import { Form, Input, InputNumber, Button, Row, Col } from "antd";
 
 const AddRooms = () => {
   const [form] = Form.useForm();
@@ -9,46 +9,69 @@ const AddRooms = () => {
   };
 
   return (
-    <Form form={form} layout="vertical" name="addRoomsForm" onFinish={onFinish}>
-      {["1rk", "1bhk", "2bhk"].map((roomType) => (
-        <React.Fragment key={roomType}>
-          <h3>{roomType.toUpperCase()} Section</h3>
-          <Form.Item
-            name={`${roomType}Count`}
-            label="Room Count"
-            rules={[
-              {
-                required: true,
-                message: `Please input the count for ${roomType.toUpperCase()} rooms!`,
-              },
-            ]}
-          >
-            <InputNumber min={1} />
-          </Form.Item>
-          <Form.Item
-            name={`${roomType}Rent`}
-            label="Rent"
-            rules={[{ required: true, message: "Please input the rent!" }]}
-          >
-            <InputNumber min={1} />
-          </Form.Item>
-          <Form.Item name={`${roomType}Description`} label="Description">
-            <Input.TextArea />
-          </Form.Item>
-          <Form.Item
-            name={`${roomType}EquipmentDetails`}
-            label="Equipment Details"
-          >
-            <Input />
-          </Form.Item>
-        </React.Fragment>
-      ))}
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <div className="add_rooms_container">
+      <Form
+        form={form}
+        layout="vertical"
+        name="addRoomsForm"
+        onFinish={onFinish}
+      >
+        {["1rk", "1bhk", "2bhk"].map((roomType) => (
+          <React.Fragment key={roomType}>
+            <h3>{roomType.toUpperCase()} Section</h3>
+            <Row gutter={16}>
+              <Col xs={24} sm={24} md={8}>
+                <Form.Item
+                  name={`${roomType}Count`}
+                  label="Room Count"
+                  rules={[
+                    {
+                      required: true,
+                      message: `Please input the count for ${roomType.toUpperCase()} rooms!`,
+                    },
+                  ]}
+                >
+                  <InputNumber min={1} style={{ width: "100%" }} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} sm={24} md={8}>
+                <Form.Item
+                  name={`${roomType}Rent`}
+                  label="Rent"
+                  rules={[
+                    { required: true, message: "Please input the rent!" },
+                  ]}
+                >
+                  <InputNumber min={1} style={{ width: "100%" }} />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col xs={24}>
+                <Form.Item name={`${roomType}Description`} label="Description">
+                  <Input.TextArea />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col xs={24}>
+                <Form.Item
+                  name={`${roomType}EquipmentDetails`}
+                  label="Equipment Details"
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+            </Row>
+          </React.Fragment>
+        ))}
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
