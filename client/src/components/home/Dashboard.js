cdb6285a95a0ae19d7942ace6b10d666554d60fa
unknown <sakarim9124@gmail.com>
2023-12-27 19:23:05 +0530

    updated

--- client/src/components/home/Dashboard.js
+++ client/src/components/home/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Card, Row, Col } from "antd";
+import { Card, Row, Col, Divider } from "antd";
 
 const Dashboard = () => {
   const roomDetails = useSelector(
@@ -8,17 +8,32 @@ const Dashboard = () => {
   );
 
   return (
-    <div className="dahboard_container">
+    <div className="dashboard-container" style={{ padding: "20px" }}>
       {roomDetails.map((property) => (
         <div key={property._id}>
           {Object.entries(property.roomTypesContainer.roomTypes).map(
-            ([roomType, { rooms }]) => (
+            ([roomType, { rooms }], index, array) => (
               <div key={roomType}>
-                <h2>{roomType.toUpperCase()} Rooms</h2>
-                <Row gutter={16}>
+                <Divider
+                  orientation="left"
+                  style={{ fontSize: "24px", fontWeight: "bold" }}
+                >
+                  {roomType.toUpperCase()} Rooms
+                </Divider>
+                <Row gutter={[16, 16]}>
+                  {" "}
+                  {/* x and y space between cards */}
                   {rooms.map((room) => (
-                    <Col key={room._id} span={8}>
-                      <Card title={room.name} bordered={false}>
+                    <Col key={room._id} xs={24} sm={12} md={8} lg={6}>
+                      <Card
+                        title={room.name}
+                        bordered={true}
+                        style={{ cursor: "pointer" }} // Add cursor pointer style here
+                        onClick={() => {
+                          // Add your click handler logic here if needed
+                          console.log("Card clicked!", room);
+                        }}
+                      >
                         <p>Rent: {room.details.rent}</p>
                         <p>Description: {room.details.description}</p>
                         {/* Add other room details here */}
@@ -26,6 +41,8 @@ const Dashboard = () => {
                     </Col>
                   ))}
                 </Row>
+                {/* Only add the bottom divider if it's not the last item */}
+                {index < array.length - 1 && <Divider />}
               </div>
             )
           )}
