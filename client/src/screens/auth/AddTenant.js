5e6b534de4d5ae762b01f3d93fdeee3187eba14b
unknown <sakarim9124@gmail.com>
2023-12-30 07:34:24 +0530

    updated

---
+++ client/src/screens/auth/AddTenant.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const AddTenant = () => {
+  return (
+    <div>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+      <h1>AddTenant</h1>
+    </div>
+  );
+};
+
+export default AddTenant;
