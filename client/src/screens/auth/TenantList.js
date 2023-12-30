7374118f431a0f7a83a7bee3aa8f5f5a52151993
unknown <sakarim9124@gmail.com>
2023-12-30 18:32:18 +0530

    updated

--- client/src/screens/auth/TenantList.js
+++ client/src/screens/auth/TenantList.js
@@ -1,40 +1,35 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { Card, Col, Row } from "antd";
 import { useGetAllTenantsQuery } from "../../apiSlices/tenantApiSlice";
 
 const TenantList = () => {
-  const dispatch = useDispatch();
   // RTK Query hook
-  const {
-    data: getAllTenants,
-    refetch,
-    isLoading: getAllTenantsLoading,
-  } = useGetAllTenantsQuery();
+  const { data: getAllTenants, isLoading: getAllTenantsLoading } =
+    useGetAllTenantsQuery();
 
-  //async
-  useEffect(() => {
-    if (getAllTenants) {
-      console.log({
-        getAllTenants,
-      });
-      //   dispatch(setPropertiesList(getAllTenants));
-    }
-  }, [getAllTenants, dispatch]);
+  if (getAllTenantsLoading) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
-      <h1>TenantList</h1>
+    <div className="tenant_list_container">
+      <h1>Tenant List</h1>
+      <Row gutter={16}>
+        {getAllTenants?.tenants?.map((tenant) => (
+          <Col span={8} key={tenant._id}>
+            <Card
+              title={`Tenant: ${tenant.personalDetails.name}`}
+              bordered={false}
+            >
+              <p>Room Type: {tenant.room.roomType}</p>
+              <p>Actual Price: {tenant.room.actualPrice}</p>
+              <p>Final Price: {tenant.room.finalPrice}</p>
+              <p>Father's Name: {tenant.personalDetails.fatherName}</p>
+              <p>Adults: {tenant.personalDetails.numberOfAdults}</p>
+              <p>Children: {tenant.personalDetails.numberOfChildren}</p>
+              <p>Advance Payment: {tenant.advancePayment}</p>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
