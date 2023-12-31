3e60aaffe51f8d2a52c901205156f471d56a8c29
unknown <sakarim9124@gmail.com>
2023-12-31 15:07:43 +0530

    updated

--- client/src/screens/auth/EditTenant.js
+++ client/src/screens/auth/EditTenant.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Form, Input, InputNumber, Button } from "antd";
 import { useGetTenantByIDQuery } from "../../apiSlices/tenantApiSlice";
 
 const EditTenant = () => {
   const { tenantId } = useParams();
-
+  const [form] = Form.useForm();
   const {
     data: tenantData,
     isLoading,
@@ -13,15 +14,26 @@ const EditTenant = () => {
   } = useGetTenantByIDQuery(tenantId);
 
   useEffect(() => {
-    // You can perform additional logic here if needed, based on tenantData
-  }, [tenantData]);
+    if (tenantData) {
+      // Set form fields with tenant data
+      form.setFieldsValue({
+        "personalDetails.name": tenantData.tenant.personalDetails.name,
+        "personalDetails.fatherName":
+          tenantData.tenant.personalDetails.fatherName,
+        "personalDetails.numberOfAdults":
+          tenantData.tenant.personalDetails.numberOfAdults,
+        "personalDetails.numberOfChildren":
+          tenantData.tenant.personalDetails.numberOfChildren,
+        "personalDetails.aadhaarCardNumber":
+          tenantData.tenant.personalDetails.aadhaarCardNumber,
+        advancePayment: tenantData.tenant.advancePayment,
+      });
+    }
+  }, [tenantData, form]);
 
-  // Handle form submission
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({ tenantId });
-    // Here you would handle the form submission
-    // Make an API call to update the tenant data
+  const handleSubmit = (values) => {
+    console.log(values);
+    // Handle form submission here
   };
 
   if (isLoading) return <div>Loading...</div>;
@@ -31,19 +43,52 @@ const EditTenant = () => {
   return (
     <div className="edit_tenant_container">
       <h1>Edit Tenant</h1>
-      <form onSubmit={handleSubmit}>
-        {/* Create form fields and populate them with tenantData */}
-        {/* Example: */}
-        <label>
-          Name:
-          <input
-            type="text"
-            value={tenantData?.personalDetails?.name} /* ... */
-          />
-        </label>
-        {/* Add other fields for editing tenant information */}
-        <button type="submit">Update Tenant</button>
-      </form>
+
+      {/* Room Details Section (Non-editable) */}
+      <div className="room_details">
+        <h2>Room Details</h2>
+        <p>Room Name: {tenantData?.roomDetails.name}</p>
+        <p>Room Type: {tenantData?.tenant.room.roomType}</p>
+        <p>Rent: {tenantData?.roomDetails.details.rent}</p>
+        <p>Description: {tenantData?.roomDetails.details.description}</p>
+        {/* Add more room details if needed */}
+      </div>
+
+      {/* Tenant Details Form */}
+      <Form form={form} layout="vertical" onFinish={handleSubmit}>
+        <Form.Item name="personalDetails.name" label="Tenant Name">
+          <Input />
+        </Form.Item>
+        <Form.Item name="personalDetails.fatherName" label="Father's Name">
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="personalDetails.numberOfAdults"
+          label="Number of Adults"
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="personalDetails.numberOfChildren"
+          label="Number of Children"
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="personalDetails.aadhaarCardNumber"
+          label="Aadhaar Card Number"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="advancePayment" label="Advance Payment">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Update Tenant
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
